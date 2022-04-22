492cbf8aa509bef0e6ad3367cb82b743517a35b2
Programming Hero <programming.hero1@gmail.com>
2022-04-22 00:28:12 -0500

    created order collection post

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('geniusCar').collection('service');
+        const orderCollection = client.db('geniusCar').collection('order');
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -48,6 +49,13 @@ async function run() {
             const result = await serviceCollection.deleteOne(query);
             res.send(result);
         });
+
+        // POST Order
+        app.post('/order', async(req, res) =>{
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.res(result);
+        })
 
     }
     finally {

f3c51997de8d3525c99d314b10c190688128b5f9
Programming Hero <programming.hero1@gmail.com>
2022-04-23 13:24:57 -0500

    created login api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -21,6 +22,16 @@ async function run() {
         const serviceCollection = client.db('geniusCar').collection('service');
         const orderCollection = client.db('geniusCar').collection('order');
         
+        // AUTH
+        app.post('/login', async(req, res) =>{
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({accessToken});
+        })
+
+        // SERVICES API
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);

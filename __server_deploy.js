7a70199922b3fbb6db554aeb6ff4f3ef63a736af
Programming Hero <programming.hero1@gmail.com>
2022-04-24 14:51:57 -0500

    adding steps

---
+++ __server_deploy.js
@@ -0,0 +1,14 @@
+/**
+ * -------------------
+ * ONE TIME for your computer
+ * -----------------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * --------------------
+ * For each project one time
+ * --------------------------
+ * 1. heroku create
+ * 
+*/
